f49c0bf04e556b239ea3ff12457b2e71d317b734
Arto Bendiken <arto@bendiken.net>
2009-08-25 03:20:54 +0200

    Implemented a compiler skeleton.

--- lib/trith/compiler.rb
+++ lib/trith/compiler.rb
@@ -1,5 +1,32 @@
 module Trith
   class Compiler
-    # TODO
+    attr_accessor :code
+
+    def self.compile(files, options = {}, &block)
+      require 'sxp' unless defined?(::SXP)
+
+      compiler = self.new(options)
+      [files].flatten.each do |file|
+        compiler << SXP.read_file(file)
+      end
+      compiler.compile!
+    end
+
+    def initialize(options = {}, &block)
+      @options = options
+
+      if block_given?
+        block.call(self)
+      end
+    end
+
+    def <<(exps)
+      @code ||= []
+      exps.respond_to?(:to_ary) ? @code += exps : @code << exps
+    end
+
+    def compile!
+      raise NotImplementedError # TODO
+    end
   end
 end
