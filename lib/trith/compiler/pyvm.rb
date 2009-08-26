5e1c3a02031c11d9948b8a78a8a5c70146541aba
Arto Bendiken <arto@bendiken.net>
2009-08-26 04:25:29 +0200

    Implemented a PyVM compiler stub.

---
+++ lib/trith/compiler/pyvm.rb
@@ -0,0 +1,43 @@
+require 'machinery/target/python'
+
+module Trith class Compiler
+  class PyVM < Compiler
+
+    def compile!
+      # TODO
+    end
+
+    def compile_nil(instruction)
+      super # TODO
+    end
+
+    def compile_boolean(instruction)
+      super # TODO
+    end
+
+    def compile_integer(instruction)
+      super # TODO
+    end
+
+    def compile_number(instruction)
+      super # TODO
+    end
+
+    def compile_string(instruction)
+      super # TODO
+    end
+
+    def compile_symbol(instruction)
+      super # TODO
+    end
+
+    def compile_quotation(instruction)
+      super # TODO
+    end
+
+    def compile_reference(instruction)
+      super # TODO
+    end
+
+  end
+end end
