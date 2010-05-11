7784d6c026521d44c274df3436c295dc309da271
Arto Bendiken <arto@bendiken.net>
2010-05-11 04:14:52 +0200

    Implemented a trith.lang.SequentialMachine class.

---
+++ src/java/trith/lang/SequentialMachine.java
@@ -0,0 +1,39 @@
+package trith.lang;
+import  java.util.concurrent.Executors;
+import  java.util.concurrent.Future;
+import  java.util.Stack;
+import  java.util.EmptyStackException;
+
+/**
+ * @author Arto Bendiken
+ */
+public class SequentialMachine extends AbstractMachine {
+  public final Stack<Future> stack = new Stack<Future>();
+  public final Stack<Future> queue = new Stack<Future>();
+
+  public SequentialMachine() {
+    super(Executors.newSingleThreadExecutor());
+  }
+
+  public Future peek() {
+    try {
+      return stack.peek();
+    }
+    catch (EmptyStackException e) {
+      return null;
+    }
+  }
+
+  public Future pop() {
+    try {
+      return stack.pop();
+    }
+    catch (EmptyStackException e) {
+      throw new StackUnderflowError();
+    }
+  }
+
+  public void push(Future value) {
+    stack.push(value);
+  }
+}
