e86271f504f4705f79d510c024613eac8bcba6c7
Arto Bendiken <arto@bendiken.net>
2010-05-11 04:15:24 +0200

    Implemented a trith.lang.ParallelMachine class.

---
+++ src/java/trith/lang/ParallelMachine.java
@@ -0,0 +1,43 @@
+package trith.lang;
+import  java.util.concurrent.Executors;
+import  java.util.concurrent.Future;
+import  java.util.Stack;
+import  java.util.EmptyStackException;
+
+/**
+ * @author Arto Bendiken
+ */
+public class ParallelMachine extends AbstractMachine {
+  public final Stack<Future> stack = new Stack<Future>();
+  public final Stack<Future> queue = new Stack<Future>();
+
+  public ParallelMachine() {
+    this(4); // TODO: figure out how many CPUs are available
+  }
+
+  public ParallelMachine(int threads) {
+    super(Executors.newFixedThreadPool(threads));
+  }
+
+  public Future peek() {
+    try {
+      return stack.peek();
+    }
+    catch (EmptyStackException e) {
+      return null;
+    }
+  }
+
+  public Future pop() {
+    try {
+      return stack.pop();
+    }
+    catch (EmptyStackException e) {
+      throw new StackUnderflowError();
+    }
+  }
+
+  public void push(Future value) {
+    stack.push(value);
+  }
+}
