fc1564279c22f7b3e02b4b12152282eb0dd0edee
Arto Bendiken <arto@bendiken.net>
2010-05-11 03:42:38 +0200

    Rebooted the library to a clean slate.

--- lib/trith.rb
+++ lib/trith.rb
@@ -1,19 +1,6 @@
-require 'uri'
-require 'trith/version'
-
-unless defined?(SXP)
-  autoload :SXP, 'sxp'
-end
-
-unless defined?(Machinery)
-  autoload :Machinery, 'machinery'
-end
+require 'digest/sha1'
+require 'rdf'
 
 module Trith
-  autoload :Compiler,    'trith/compiler'
-  autoload :Instruction, 'trith/instruction'
-  autoload :Machine,     'trith/machine'
-  autoload :Optimizer,   'trith/optimizer'
-  autoload :Program,     'trith/program'
-  autoload :Reader,      'trith/reader'
+  autoload :VERSION, 'trith/version'
 end
--- lib/trith/compiler/mlvm.rb
+++
@@ -1,9 +0,0 @@
-require 'trith/compiler/jvm'
-
-module Trith class Compiler
-  class MLVM < JVM
-    BiteScript.bytecode_version = BiteScript::JAVA1_7
-
-    # TODO: an invokedynamic-enabled version of the JVM compiler.
-  end
-end end
