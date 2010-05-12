0eda22fb323bd5da652326810879686da5e7b01b
Arto Bendiken <arto@bendiken.net>
2010-05-12 04:01:51 +0200

    Added a Unicode code points versus code units reference to trith.stream.length.

--- src/java/trith/stream/length.java
+++ src/java/trith/stream/length.java
@@ -12,7 +12,11 @@ import  java.util.Collection;
 public class length extends UnaryStreamOperator<BigDecimal, Object> {
   public static final String URL = "http://trith.org/stream/length";
 
+  /**
+   * @see http://java.sun.com/javase/6/docs/api/java/lang/Character.html#unicode
+   */
   public BigDecimal execute(String str) {
+    //return Number.valueOf(str.codePointCount(0, str.length())); // TODO
     return Number.valueOf(str.length());
   }
 
