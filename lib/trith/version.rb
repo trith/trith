35824e0d0f351248d71e044d2f6df6f39a91e06d
Arto Bendiken <arto@bendiken.net>
2010-06-18 05:17:35 +0200

    Bumped the version to 0.0.4.

--- lib/trith/version.rb
+++ lib/trith/version.rb
@@ -2,11 +2,10 @@ module Trith
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 3
+    TINY  = 4
     EXTRA = nil
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
-    STRING << ".#{EXTRA}" if EXTRA
+    STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
 
     ##
     # @return [String]
