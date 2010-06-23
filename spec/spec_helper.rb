f3b75ce931ea513bf4b21a7085ea33ed9ca03d20
Arto Bendiken <arto@bendiken.net>
2010-06-23 11:31:12 +0200

    Added a #underflow_stack RSpec matcher.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 require 'trith'
 include Trith
+
+module Trith
+  module Spec
+    module Matchers
+      def self.define(name, &declarations)
+        define_method name do |*expected|
+          ::Spec::Matchers::Matcher.new(name, *expected, &declarations)
+        end
+      end
+
+      define :underflow_stack do
+        match do |proc|
+          proc.should raise_error(Machine::StackUnderflowError)
+        end
+      end
+    end
+  end
+end
+
+Spec::Runner.configure do |config|
+  config.include(Trith::Spec::Matchers)
+end
