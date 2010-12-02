2620b058d97a73ae9067fff2f705ab2f98d4c433
Arto Bendiken <arto@bendiken.net>
2010-12-02 01:33:31 +0100

    Defined the low-level trith_machine data structure.

--- src/trith.c
+++ src/trith.c
@@ -12,3 +12,64 @@
 // Constants
 
 const char* const trith_version_string = PACKAGE_VERSION;
+
+//////////////////////////////////////////////////////////////////////////////
+// Machine API
+
+trith_machine*
+trith_machine_alloc() {
+  return trith_slice_alloc(sizeof(trith_machine));
+}
+
+trith_machine*
+trith_machine_new() {
+  trith_machine* machine = trith_machine_alloc();
+  trith_machine_init(machine);
+  return machine;
+}
+
+trith_machine*
+trith_machine_copy(const trith_machine* machine) {
+  assert(machine != NULL);
+  trith_machine* copy = trith_machine_new();
+  // copy the stack:
+  for (GList* list = machine->stack.head; list != NULL; list = list->next) {
+    g_queue_push_tail(&copy->stack, list->data);
+  }
+  // copy the queue:
+  for (GList* list = machine->queue.head; list != NULL; list = list->next) {
+    g_queue_push_tail(&copy->queue, list->data);
+  }
+  // copy the context:
+  copy->context.parent = machine->context.parent;
+  GHashTableIter iter;
+  gpointer key, value;
+  g_hash_table_iter_init(&iter, machine->context.locals);
+  while (g_hash_table_iter_next(&iter, &key, &value) != FALSE) {
+    g_hash_table_insert(copy->context.locals, &key, &value);
+  }
+  return copy;
+}
+
+void
+trith_machine_init(trith_machine* machine) {
+  assert(machine != NULL);
+  g_queue_init(&machine->stack);
+  g_queue_init(&machine->queue);
+  machine->context.parent = NULL;
+  machine->context.locals = g_hash_table_new(NULL, NULL);
+}
+
+void
+trith_machine_free(trith_machine* machine) {
+  assert(machine != NULL);
+  g_queue_clear(&machine->stack);
+  g_queue_clear(&machine->queue);
+  if (machine->context.locals) {
+    g_hash_table_destroy(machine->context.locals);
+  }
+  trith_slice_free(sizeof(trith_machine), machine);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Operators
--- src/trith.h
+++ src/trith.h
@@ -18,16 +18,44 @@ extern "C" {
 extern const char* const trith_version_string;
 
 //////////////////////////////////////////////////////////////////////////////
+// Machine API
+
+typedef GQueue trith_stack;
+typedef GQueue trith_queue;
+typedef GHashTable trith_table;
+
+typedef struct {
+  struct trith_context* parent;
+  trith_table* locals;
+} trith_context;
+
+typedef struct {
+  trith_stack stack;
+  trith_queue queue;
+  trith_context context;
+  void* user_data;
+} trith_machine;
+
+extern trith_machine* trith_machine_alloc();
+extern trith_machine* trith_machine_new();
+extern trith_machine* trith_machine_copy(const trith_machine* machine);
+extern void trith_machine_init(trith_machine* machine);
+extern void trith_machine_free(trith_machine* machine);
+
+//////////////////////////////////////////////////////////////////////////////
+// Operators
+
+//////////////////////////////////////////////////////////////////////////////
 // Utilities
 
-#define trith_malloc       g_malloc0
-#define trith_calloc       g_malloc0_n
-#define trith_realloc      g_realloc
-#define trith_free         g_free
-#define trith_slice_alloc  g_slice_alloc0
-#define trith_slice_copy   g_slice_copy
-#define trith_slice_free1  g_slice_free1
-#define trith_memmove      g_memmove
+#define trith_malloc      g_malloc0
+#define trith_calloc      g_malloc0_n
+#define trith_realloc     g_realloc
+#define trith_free        g_free
+#define trith_slice_alloc g_slice_alloc0
+#define trith_slice_copy  g_slice_copy
+#define trith_slice_free  g_slice_free1
+#define trith_memmove     g_memmove
 
 //////////////////////////////////////////////////////////////////////////////
 // Miscellaneous
