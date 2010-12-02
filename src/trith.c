e2aa05d35eb2a2ac09371f435f561938680f1f94
Arto Bendiken <arto@bendiken.net>
2010-12-02 02:29:25 +0100

    Implemented the basic control and stack operators in C.

--- src/trith.c
+++ src/trith.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <strings.h>
 #include <math.h>
+
+#define __TRITH_INIT__
 #include "trith.h"
 #include "config.h"
 
@@ -12,6 +14,62 @@
 // Constants
 
 const char* const trith_version_string = PACKAGE_VERSION;
+
+trith_value trith_false = { .type = TRITH_BOOL, .value.boolean = FALSE };
+trith_value trith_true  = { .type = TRITH_BOOL, .value.boolean = TRUE };
+
+//////////////////////////////////////////////////////////////////////////////
+// Machine internals
+
+static inline void
+trith_stack_init(trith_stack* stack) {
+  assert(stack != NULL);
+  g_queue_init(stack);
+}
+
+static inline void
+trith_stack_clear(trith_stack* stack) {
+  assert(stack != NULL);
+  // TODO: deallocate values
+  g_queue_clear(stack);
+}
+
+static inline guint
+trith_stack_size(const trith_stack* stack) {
+  return g_queue_get_length((trith_stack*)stack);
+}
+
+static inline bool
+trith_stack_is_empty(const trith_stack* stack) {
+  return trith_stack_size(stack) == 0;
+}
+
+static inline trith_value*
+trith_stack_peek(const trith_stack* stack) {
+  assert(stack != NULL);
+  return g_queue_peek_head((trith_stack*)stack);
+}
+
+static inline trith_value*
+trith_stack_pop(trith_stack* stack) {
+  assert(stack != NULL);
+  return g_queue_pop_head(stack);
+}
+
+static inline void
+trith_stack_push(trith_stack* stack, trith_value* value) {
+  assert(stack != NULL);
+  g_queue_push_head(stack, value);
+}
+
+static inline bool
+trith_ensure_stack_size(trith_machine* machine, size_t size) {
+  if (trith_stack_size(&machine->stack) < size) {
+    machine->error = TRITH_ERROR_STACK_UNDERFLOW;
+    return FALSE;
+  }
+  return TRUE;
+}
 
 //////////////////////////////////////////////////////////////////////////////
 // Machine API
@@ -54,8 +112,9 @@ trith_machine_copy(const trith_machine* machine) {
 void
 trith_machine_init(trith_machine* machine) {
   assert(machine != NULL);
-  g_queue_init(&machine->stack);
-  g_queue_init(&machine->queue);
+  machine->error = TRITH_ERROR_NONE;
+  trith_stack_init(&machine->stack);
+  trith_stack_init(&machine->queue);
   machine->context.parent = NULL;
   machine->context.locals = g_hash_table_new(NULL, NULL);
 }
@@ -63,13 +122,144 @@ trith_machine_init(trith_machine* machine) {
 void
 trith_machine_free(trith_machine* machine) {
   assert(machine != NULL);
-  g_queue_clear(&machine->stack);
-  g_queue_clear(&machine->queue);
+  trith_stack_clear(&machine->stack);
+  trith_stack_clear(&machine->queue);
   if (machine->context.locals) {
     g_hash_table_destroy(machine->context.locals);
   }
   trith_slice_free(sizeof(trith_machine), machine);
 }
 
+trith_value*
+trith_machine_peek(const trith_machine* machine) {
+  assert(machine != NULL);
+  return trith_stack_peek(&machine->stack);
+}
+
+trith_value*
+trith_machine_pop(trith_machine* machine) {
+  assert(machine != NULL);
+  return trith_stack_pop(&machine->stack);
+}
+
+void
+trith_machine_push(trith_machine* machine, trith_value* value) {
+  assert(machine != NULL);
+  trith_stack_push(&machine->stack, value);
+}
+
 //////////////////////////////////////////////////////////////////////////////
-// Operators
+// Control operators
+
+void
+trith_op_halt(trith_machine* machine) {
+  assert(machine != NULL);
+  trith_stack_clear(&machine->queue);
+}
+
+void
+trith_op_reset(trith_machine* machine) {
+  assert(machine != NULL);
+  machine->error = TRITH_ERROR_NONE;
+  trith_stack_clear(&machine->stack);
+  trith_stack_clear(&machine->queue);
+  g_hash_table_remove_all(machine->context.locals);
+}
+
+void
+trith_op_nop(trith_machine* machine) {
+  assert(machine != NULL);
+  // do nothing
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Stack operators
+
+void
+trith_op_clear(trith_machine* machine) {
+  assert(machine != NULL);
+  trith_stack_clear(&machine->stack);
+}
+
+void
+trith_op_depth(trith_machine* machine) {
+  assert(machine != NULL);
+  trith_value* depth = trith_malloc(sizeof(trith_value));
+  depth->value.integer = trith_stack_size(&machine->stack);
+  trith_stack_push(&machine->stack, depth);
+}
+
+void
+trith_op_pop(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 1)) {
+    trith_stack_pop(&machine->stack);
+  }
+}
+
+void
+trith_op_pop2(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 2)) {
+    trith_stack_pop(&machine->stack);
+    trith_stack_pop(&machine->stack);
+  }
+}
+
+void
+trith_op_pop3(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 3)) {
+    trith_stack_pop(&machine->stack);
+    trith_stack_pop(&machine->stack);
+    trith_stack_pop(&machine->stack);
+  }
+}
+
+void
+trith_op_dup(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 1)) {
+    trith_stack_push(&machine->stack, trith_stack_peek(&machine->stack));
+  }
+}
+
+void
+trith_op_dup2(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 2)) {
+    trith_value* a = trith_stack_pop(&machine->stack);
+    trith_value* b = trith_stack_pop(&machine->stack);
+    trith_stack_push(&machine->stack, b);
+    trith_stack_push(&machine->stack, a);
+    trith_stack_push(&machine->stack, b);
+    trith_stack_push(&machine->stack, a);
+  }
+}
+
+void
+trith_op_dup3(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 3)) {
+    trith_value* a = trith_stack_pop(&machine->stack);
+    trith_value* b = trith_stack_pop(&machine->stack);
+    trith_value* c = trith_stack_pop(&machine->stack);
+    trith_stack_push(&machine->stack, c);
+    trith_stack_push(&machine->stack, b);
+    trith_stack_push(&machine->stack, a);
+    trith_stack_push(&machine->stack, c);
+    trith_stack_push(&machine->stack, b);
+    trith_stack_push(&machine->stack, a);
+  }
+}
+
+void
+trith_op_swap(trith_machine* machine) {
+  assert(machine != NULL);
+  if (trith_ensure_stack_size(machine, 2)) {
+    trith_value* a = trith_stack_pop(&machine->stack);
+    trith_value* b = trith_stack_pop(&machine->stack);
+    trith_stack_push(&machine->stack, a);
+    trith_stack_push(&machine->stack, b);
+  }
+}
--- src/trith.h
+++ src/trith.h
@@ -13,12 +13,7 @@ extern "C" {
 #include <glib.h>
 
 //////////////////////////////////////////////////////////////////////////////
-// Constants
-
-extern const char* const trith_version_string;
-
-//////////////////////////////////////////////////////////////////////////////
-// Machine API
+// Types
 
 typedef GQueue trith_stack;
 typedef GQueue trith_queue;
@@ -29,21 +24,77 @@ typedef struct {
   trith_table* locals;
 } trith_context;
 
+typedef enum {
+  TRITH_ERROR_NONE = 0,
+  TRITH_ERROR_STACK_UNDERFLOW,
+} trith_error;
+
 typedef struct {
+  trith_error error;
   trith_stack stack;
   trith_queue queue;
   trith_context context;
   void* user_data;
 } trith_machine;
 
+typedef enum {
+  TRITH_NIL = 0,
+  TRITH_BOOL,
+  TRITH_INT,
+  TRITH_LIST,
+  TRITH_OP,
+} trith_type;
+
+typedef void (*trith_op)(trith_machine* machine);
+
+typedef struct {
+  trith_type type;
+  union value {
+    bool boolean;
+    gint integer;
+  } value;
+} trith_value;
+
+//////////////////////////////////////////////////////////////////////////////
+// Constants
+
+extern const char* const trith_version_string;
+
+#ifndef __TRITH_INIT__
+extern const trith_value trith_false;
+extern const trith_value trith_true;
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// Machine
+
 extern trith_machine* trith_machine_alloc();
 extern trith_machine* trith_machine_new();
 extern trith_machine* trith_machine_copy(const trith_machine* machine);
 extern void trith_machine_init(trith_machine* machine);
 extern void trith_machine_free(trith_machine* machine);
+extern trith_value* trith_machine_peek(const trith_machine* machine);
+extern trith_value* trith_machine_pop(trith_machine* machine);
+extern void trith_machine_push(trith_machine* machine, trith_value* value);
 
 //////////////////////////////////////////////////////////////////////////////
 // Operators
+
+// Control operators
+extern void trith_op_halt(trith_machine* machine);
+extern void trith_op_reset(trith_machine* machine);
+extern void trith_op_nop(trith_machine* machine);
+
+// Stack operators
+extern void trith_op_clear(trith_machine* machine);
+extern void trith_op_depth(trith_machine* machine);
+extern void trith_op_pop(trith_machine* machine);
+extern void trith_op_pop2(trith_machine* machine);
+extern void trith_op_pop3(trith_machine* machine);
+extern void trith_op_dup(trith_machine* machine);
+extern void trith_op_dup2(trith_machine* machine);
+extern void trith_op_dup3(trith_machine* machine);
+extern void trith_op_swap(trith_machine* machine);
 
 //////////////////////////////////////////////////////////////////////////////
 // Utilities
